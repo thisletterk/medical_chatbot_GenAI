3c4b75c0aa810e187e7a8c59c850484827e5ca13
Kingsley Omokwe <user@Kingsley-Omokwe.local>
2024-11-15 15:46:56 +0000

    project struture

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'MedChat GenAI Project',
+    version= '0.0.0',
+    author= 'Kingsley Omokwe',
+    author_email= 'omokwek@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
